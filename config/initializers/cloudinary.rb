3c5f40cf2f240a802940277652bb41986398ec57
priya shankar <preiyah@gmail.com>
2022-02-18 19:59:10 +0100

    again

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dqpbsevz8'
+  config.api_key = '466915785248887'
+  config.api_secret = 'bKqYsL8uxJzrxV1DXhKYDVPU80o'
+  config.secure = true
+  config.cdn_subdomain = true
+end
